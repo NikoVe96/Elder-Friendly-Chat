7243e765f6322119a7abe93dd82e276f0637f400
Nikoline Vejby <nikolinevejby96@gmail.com>
2023-11-02 12:05:45 +0100

    Create project and insert main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import logo from './images/logo.jpg';
-import './App.css';
+import './Styles.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <div class="container">
+        <div class="centered-content">
+          <image class="logoImage" src="/src/images/logo.jpg" alt="Logo"></image>
+          <h1> Elder Friendly Chat</h1>
+
+          <div class="centered-buttons">
+            <img class="img" src="/src/images/conversations1.png" alt="conversations"></img>
+            <a href="ConversationsList.html"><button class="button"> View conversations</button></a>
+          </div>
+
+          <div class="centered-buttons">
+            <img class="img" src="/src/images/add_contact.png" alt="conversations"></img>
+            <button class="button">Add new contact</button>
+          </div>
+        </div>
+
+        <div>
+          <button class="settingsButton">Settings</button>
+        </div>
+
+        <div>
+          <a href="MainMenu.html" class="home-button">Home</a>
+        </div>
+      </div>
+    </>
   );
 }
 
